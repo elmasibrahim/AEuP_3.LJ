530aec121c083caa7ed79b16f256852c208e4ae3
elmasibrahim <elmas.ibrahim05@yahoo.com>
2023-10-09 11:25:38 +0200

    Polymorphie-Aufgabe Datagenerator

--- Testdatengenerator Vererbung/src/Main.java
+++ Testdatengenerator Vererbung/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("CSV oder XML?: ");
+        String choice = scanner.nextLine();
+        DataGenerator generator = new DataGenerator();
+        switch (choice) {
+            case "CSV":
+                generator = new DataGeneratorCSV(); //Upcasting
+                ((DataGeneratorCSV)generator).saveData(10, 20); //Downcasting
+                break;
+            case "XML":
+                generator = new DataGeneratorXML(); //Upcasting
+                ((DataGeneratorXML)generator).saveData(10, 20); //Downcasting
+                break;
+            default:
+                System.out.println("Bitte etwas richtiges eingeben");
+        }
+
 
     }
 }
